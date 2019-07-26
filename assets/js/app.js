a037ecee8a6e2e4906e401016a25bc26e7bb6c50
Fernando Fagonde <fernando@ipsillon.cc>
2019-07-25 23:09:17 -0300

    balao

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,7 +16,7 @@ particlesJS('particles-js',
   {
     "particles": {
       "number": {
-        "value": 120,
+        "value": 50,
         "density": {
           "enable": true,
           "value_area": 800
